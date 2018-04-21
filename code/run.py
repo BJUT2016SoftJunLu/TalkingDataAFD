2858ce32ee6a75f90b5be67e98c160741f84bd1a
Chen Wen <chenwen@wingspan.cn>
2018-04-21 12:35:09 +0800

    code

--- code/run.py
+++ code/run.py
@@ -2,12 +2,12 @@ import featrue_one
 import model_xgboost
 import model_lightgbm
 
-result_path_lightgbm = '../data/result_lightgbm_2.csv'
-result_path_xgboost = '../data/result_xgboost.csv'
-
-test_data_path = '../data/fe_test.csv'
-train_data_path = '../data/fe_train.csv'
-val_data_path = '../data/fe_val.csv'
+# result_path_lightgbm = '../data/result_lightgbm_2.csv'
+# result_path_xgboost = '../data/result_xgboost.csv'
+#
+# test_data_path = '../data/fe_test.csv'
+# train_data_path = '../data/fe_train.csv'
+# val_data_path = '../data/fe_val.csv'
 
 def main():
     train_data, val_data, test_data = featrue_one.apply()
