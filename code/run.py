57046c01c59ac1df767b423272edfef7bd94a1f2
Chen Wen <chenwen@wingspan.cn>
2018-04-21 18:50:59 +0800

    code

--- code/run.py
+++ code/run.py
@@ -1,6 +1,7 @@
 import featrue_one
 import model_xgboost
 import model_lightgbm
+import pandas as pd
 
 result_path_lightgbm = '../data/result_lightgbm_2.csv'
 result_path_xgboost = '../data/result_xgboost.csv'
@@ -9,12 +10,28 @@ test_data_path = '../data/fe_test.csv'
 train_data_path = '../data/fe_train.csv'
 val_data_path = '../data/fe_val.csv'
 
-def main():
-    train_data, val_data, test_data = featrue_one.apply()
 
-    train_data.to_csv(train_data_path)
-    val_data.to_csv(val_data_path)
-    test_data.to_csv(test_data_path)
+predictors = []
+predictors.extend(['app', 'device', 'os', 'channel', 'hour', 'day',
+                   'ip_tcount', 'ip_tchan_count', 'ip_app_count',
+                   'ip_app_os_count', 'ip_app_os_var',
+                   'ip_app_channel_var_day', 'ip_app_channel_mean_hour', 'nextClick', 'nextClick_shift'])
+
+for i in range(0, 9):
+    predictors.append("X%d" % (i))
+
+def main():
+    # train_data, val_data, test_data = featrue_one.apply()
+    #
+    # train_data.to_csv(train_data_path)
+    # val_data.to_csv(val_data_path)
+    # test_data.to_csv(test_data_path)
+    train_data = pd.read_csv(train_data_path,
+                             usecols=predictors)
+    val_data = pd.read_csv(val_data_path,
+                             usecols=predictors)
+    test_data = pd.read_csv(test_data_path,
+                             usecols=predictors)
 
 
     xgboost_model = model_xgboost.model_train(train_data)
